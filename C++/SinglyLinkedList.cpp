1d23685422ea988a70e4feb00bc0a99092d686c5
DavidTesar <73195563+DavidTesar@users.noreply.github.com>
2021-10-28 13:25:09 -0500

    Rule of three added

--- C++/SinglyLinkedList.cpp
+++ C++/SinglyLinkedList.cpp
@@ -1,37 +1,91 @@
 #include<iostream>
 using namespace std;
 
+//Single node, using template for high reusibility within the code
+template <class T>
+class SNode{
+public:
+    const T& getElement() const{return this.element;}
+    SNode<E>* getNext() const {return this.next;}
+private:
+    T element;
+    SNode<T>* next;
+friend class SLinkedList<T>;
+
+}
+
 ///Here we are creating a Node for Linked List
-class Node
+template <class T>
+class SLinkedList
 {
 public:
-    int Data;
-    Node* next;
+    //Constructor
+    SLinkedList();
 
-    Node(int val) : head(NULL), tail(NULL)
-    {
-        Data=val;
-        next=NULL;
-    }
-    void InsertAtTail(Node* &head,int value);
-    void InsertAtHead(Node* &head,int value);
-    void Display(Node* head);
-    bool Search(Node* head,int Value);
+    //Copy constructor
+    SLinkedList(SLinkedList<T>&);
+
+    //Destructor
+    ~SLinkedList();
+
+    //Assignment overload
+    SLinkedList<T>& operator=(SLinkedList<T>&);
+
+    //returns true if list is empty
     bool empty() const;
+
+    //returns element at the head
+    const T& front();
+
+
+    void InsertAtTail(SLinkedList* &head,int value);
+    void InsertAtHead(SLinkedList* &head,int value);
+    void Display(SLinkedList* head);
+    bool Search(SLinkedList* head,int Value);
     void removeFront();
 
-    Node* getHead() const { return head; };   // return head address
-    Node* getTail() const { return tail; };
+    SLinkedList* getHead() const { return head; };   // return head address
+    SLinkedList* getTail() const { return tail; };
 
 
 private:
-    int* head;
-    int* tail;
+    SNode<T>* head;
+    SNode<T>* tail;
     
 };
 
+template <class T>
+const T& SLinkedList::front(){
+    return this.head->element;
+}
+
+///Constructor
+template <class T>
+SLinkedList::SLinkedList():head(NULL),tail(NULL){}
+
+
+
+//operator = overloading
+template <class T>
+SLinkedList<T>& SLinkedList<T>::operator=(SLinkedList<T>& rhs) // assignment operator
+{
+
+    if(this != rhs)//protection against multinclusion
+    {
+        //empty list
+        while(head != NULL){removeFront();}
+        for(SNode<T>* curr = rhs.head; curr != NULL; curr = curr.getNext()){
+
+
+        }
+
+
+    }
+
+}
+
 ///Destructor avoiding memory leak, deletes every element in the array until head == NULL
-Node::~Node(){
+SLinkedList::~SLinkedList(){
     while(!empty()){
 
         delete removeFront();
@@ -39,7 +93,7 @@ Node::~Node(){
 }
 
 ///Removes element at head and reassigns the pointer to the next element in the list 
-Node::removeFront(){
+SLinkedList::removeFront(){
 
     Node *old = head;
     head = old->next;
@@ -50,36 +104,37 @@ Node::removeFront(){
 }
 
 ///Here we are returning true if list is empty, otherwise false
-Node::empty(){
+SLinkedList::empty(){
 
     return head == NULL;
 }
 
 ///Here we are inserting a node at the End of the Linked List.
-Node::void InsertAtTail(Node* &head,int value)
+template <class T>
+SLinkedList::void InsertAtTail(const T& value)
 {
 
-    Node* n=new Node(value);
+    SNode<T>* n = new SNode<T>;
+    n.element = value;
+    n.next = NULL;
 
-    if(head==NULL)
+    if(tail==NULL)
     {
-        head=n;
+        n.next = tail;
+        tail = n;
+        head = n;
         return;
     }
     else
     {
-        Node *temp =head;
-        while(temp->next!=NULL)
-        {
-            temp=temp->next;
-        }
-        temp->next=n;
-
+        tail.next = n;
+        tail = tail.next;
+        tail.next = NULL;
     }
 }
 
 ///Here we are inserting node at the start of linked list.
-Node::void InsertAtHead(Node* &head,int value)
+SLinkedList::void InsertAtHead(SLinkedList* &head,int value)
 {
     Node* n=new Node(value);
 
@@ -88,7 +143,7 @@ Node::void InsertAtHead(Node* &head,int value)
 }
 
 ///Here we are displaying the whole linked list.
-Node::void Display(Node* head)
+SLinkedList::void Display(SLinkedList* head)
 {
     Node* n=head;
     while(n!=NULL)
@@ -101,7 +156,7 @@ Node::void Display(Node* head)
 }
 
 ///Here we are searching for a particular element in the linked list.
-Node::bool Search(Node* head,int Value)
+SLinkedList::bool Search(SLinkedList* head,int Value)
 {
     Node* n=head;
     while(n->next!=NULL)
@@ -116,7 +171,7 @@ Node::bool Search(Node* head,int Value)
 
 int main()
 {
-    Node *head=NULL;
+    SLinkedList *head=NULL;
 
     InsertAtTail(head,1);
     InsertAtTail(head,2);
