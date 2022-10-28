0b8e07d7458c93f8b6c0fbf0bdd68c59f7318abc
Tarun Verma <59416189+Tarunverma504@users.noreply.github.com>
2022-10-28 16:06:25 +0530

    Updated logic of if condition

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -2,9 +2,11 @@ import java.util.*;
 public class Main
 {  
    static int n1=0,n2=1,n3=0;   
+   static int x;
+   x=0;
    static void printFibonacci(int n)
    {    
-        if(n>0)
+        if(n>=1)
         {    
             n3 = n1 + n2;    
             System.out.print(" "+n3);   
